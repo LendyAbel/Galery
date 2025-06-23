63ffd080c01391e60988683068663751a50b7080
LendyAbel <lendyabel93@gmail.com>
2025-06-23 14:17:31 +0200

    Frontend first commit to create branch

---
+++ frontend/src/components/Gallery/Gallery.jsx
@@ -0,0 +1,11 @@
+import './gallery.css'
+
+const Gallery = () => {
+  return (
+    <div className='gallery-container'>
+      Gallery
+    </div>
+  )
+}
+
+export default Gallery
---
+++ frontend/src/components/Upload/Upload.jsx
@@ -0,0 +1,9 @@
+import './upload.css'
+
+const Upload = () => {
+  return (
+    <div className='upload-container'>Upload</div>
+  )
+}
+
+export default Upload
