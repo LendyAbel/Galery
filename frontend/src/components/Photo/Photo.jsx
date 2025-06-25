35b318e979f47c1f6ec9ef4b6fe4c0ec142efe6e
LendyAbel <lendyabel93@gmail.com>
2025-06-25 11:44:40 +0200

    Working on UI for delete button

--- frontend/src/components/Photo/Photo.jsx
+++ frontend/src/components/Photo/Photo.jsx
@@ -4,6 +4,13 @@ const Photo = ({ photo }) => {
   return (
     <div className='photo-card'>
       <img className='photo-image' src={photo} alt='uploaded'></img>
+      <div className='delete-button'>
+        <img
+          src='/images/icons8-eliminar-50.png'
+          alt='delete'
+          className='delete-icon'
+        />
+      </div>
     </div>
   )
 }
