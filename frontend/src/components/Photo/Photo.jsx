2e67d69584391ce9072acbfc4dc0213a542a50b5
LendyAbel <lendyabel93@gmail.com>
2025-06-26 17:04:25 +0200

    Changing photos UI and adding download button

--- frontend/src/components/Photo/Photo.jsx
+++ frontend/src/components/Photo/Photo.jsx
@@ -1,20 +1,49 @@
+import { useState } from 'react'
+import Modal from 'react-modal'
+import { FaTrash, FaDownload, FaTimes } from 'react-icons/fa'
 import './photo.css'
 
+Modal.setAppElement('#root')
+
 const Photo = ({ photo, deletePhoto }) => {
-  const handleDeleteClick = ()=>{
-    deletePhoto(photo)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const clickToOpenModal = () => {
+    setIsModalOpen(true)
   }
+
+  const handleDeleteClick = () => {
+    deletePhoto(photo)
+    setIsModalOpen(false)
+  }
+
+  const handleDownload = () => {}
+
   return (
-    <div className='photo-card'>
-      <img className='photo-image' src={photo.url} alt='uploaded'></img>
-      <div className='delete-button' onClick={handleDeleteClick}>
-        <img
-          src='/images/icons8-eliminar-50.png'
-          alt='delete'
-          className='delete-icon'
-        />
+    <>
+      <div className='photo-card' onClick={clickToOpenModal}>
+        <img className='photo-image' src={photo.url} alt='gallery-photo'></img>
       </div>
-    </div>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={() => setIsModalOpen(false)}
+        className='modal'
+        overlayClassName='overlay'
+      >
+        <img className='modal-image' src={photo.url} alt='modal-photo' />
+        <div className='modal-actions'>
+          <button onClick={handleDeleteClick} title='Eliminar'>
+            <FaTrash />
+          </button>
+          <button onClick={handleDownload} title='Descargar'>
+            <FaDownload />
+          </button>
+          <button onClick={() => setIsModalOpen(false)} title='Cerrar'>
+            <FaTimes />
+          </button>
+        </div>
+      </Modal>
+    </>
   )
 }
 
